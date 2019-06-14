db2b3d57cdd7f6059a27550a05a00321d6c76f61
Anilkumar <anilskalyane@users.noreply.github.com>
2019-06-15 00:59:45 +0530

    Update autoscale-vm-creation-p.sh

--- autoscale-vm-creation-p.sh
+++ autoscale-vm-creation-p.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # CentOS 7.6
 sudo yum install mod_ssl -y
+sudo yum update kernel -y
 sudo rm -rf /etc/httpd/conf.d/ssl.conf
 sudo sed -i "17i Port 22" /etc/ssh/sshd_config
 sudo sed -i "21i Port 2266" /etc/ssh/sshd_config
